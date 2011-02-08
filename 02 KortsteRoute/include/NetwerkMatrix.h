c6162f1e465e76f057f3d6a7f329387089c90aff
Nick Overdijk <nick@dotsimplicity.net>
2011-02-08 20:26:42 +0100

    c++ and codeblocks ftw

---
+++ 02 KortsteRoute/include/NetwerkMatrix.h
@@ -0,0 +1,18 @@
+#ifndef NETWERKMATRIX_H
+#define NETWERKMATRIX_H
+
+
+class NetwerkMatrix {
+public:
+    NetwerkMatrix(std::string fileName) {
+
+    }
+    virtual ~NetwerkMatrix() {
+
+    }
+
+protected:
+private:
+};
+
+#endif // NETWERKMATRIX_H
