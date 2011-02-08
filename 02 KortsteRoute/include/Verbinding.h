f62c0f731368730bc03400eabedebef0c86f635c
Nick Overdijk <nick@dotsimplicity.net>
2011-02-08 21:44:00 +0100

    BAM verbinding.h

---
+++ 02 KortsteRoute/include/Verbinding.h
@@ -0,0 +1,70 @@
+#ifndef VERBINDING_H
+#define VERBINDING_H
+
+#include "Netwerk.h"
+
+#include <limits>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <stdexcept>
+
+
+class Verbinding : public Netwerk {
+public:
+    Verbinding(std::string fileName) {
+        std::ifstream file(fileName);
+        if (!file) throw std::runtime_error("Ik kon het bestand "+fileName+" niet lezen");
+
+        file >> stations;
+
+        std::cout << "Dit netwerk heeft " << stations << " stations" << std::endl;
+
+        while (true) {
+            int van, naar, afstand;
+            file >> van >> naar >> afstand;
+
+            if (!file) break;
+
+            std::cout << "Er is een spoor van " << van << " naar " << naar << " met lengte " << afstand << std::endl;
+            setAfstand(van, naar, afstand); // -1 wegens array indices
+        }
+
+    }
+
+    virtual ~Verbinding() {}
+
+    int getGrootte() const {
+        if (stations > 0)
+            return stations;
+        else
+            throw std::logic_error("Leeg netwerk");
+    }
+
+    int getAfstand(int van, int naar) const {
+        if (van >= 1 && van < stations && naar >= 1 && naar < stations)
+            return afstand[van][naar];
+        else {
+            std::stringstream stream("Illegaal station nummer in ");
+            stream << van+1 << ", " << naar+1;
+            throw std::out_of_range(stream.str());
+        }
+    }
+
+private:
+    int stations;
+    std::map<int <std::map<int, int>> afstand;
+
+    void setAfstand(int van, int naar, int lengte) {
+        if (van >= 1 && van < stations && naar >= 1 && naar < stations) {
+            afstand[van][naar] = lengte;
+            afstand[naar][van] = lengte;
+        } else {
+            std::stringstream stream("Illegaal station nummer in ");
+            stream << van << ", " << naar;
+            throw std::out_of_range(stream.str());
+        }
+    }
+};
+
+#endif // VERBINDING_H
