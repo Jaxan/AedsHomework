aeb72d18812d208313393f4cfdf052aa0c06d1a7
Joshua Moerman <joshua@keesmoerman.nl>
2011-02-15 23:02:10 +0100

    uberstack implemented in main

--- 03 Uno/main.cpp
+++ 03 Uno/main.cpp
@@ -12,25 +12,31 @@ std::ostream& operator<< (std::ostream& out, const std::vector<T>& x) {
 int main(int argc, char* argv[]) {
 
 	try {
-		JN::List<Card> s[3];
-		for(size_t i = 0; i < 10; ++i){
-		    Card card(Card::Yellow, i);
-            s[0].push(card);
-            s[1].push_front(card);
-            s[2].push_back(card);
+		JN::List<Card> uberStack;
+		for(size_t i = 0; i <= 12; ++i){	// de getallen + 3 speciale kaarten (van elke kleur 2 stuks)
+		    {Card card(Card::Yellow, i);
+            uberStack.push(card);
+            uberStack.push(card);}
+		    {Card card(Card::Green, i);
+            uberStack.push(card);
+            uberStack.push(card);}
+		    {Card card(Card::Red, i);
+            uberStack.push(card);
+            uberStack.push(card);}
+		    {Card card(Card::Blue, i);
+            uberStack.push(card);
+            uberStack.push(card);}
+		}
+		{
+			Card choice(Card::Choice);
+			for(size_t i = 0; i < 4; ++i) uberStack.push(choice);
+			Card choicePick4(Card::ChoicePick4);
+			for(size_t i = 0; i < 4; ++i) uberStack.push(choicePick4);
 		}
 
-		Card card(Card::Choice);
+        std::cout << "De random stapel: " << uberStack << std::endl;
 
-        s[0].push(card);
-        s[1].push_front(card);
-        s[2].push_back(card);
-
-        std::cout << "De random stapel: " << s[0] << std::endl;
-        std::cout << "De omgekeerde stapel: " << s[1] << std::endl;
-        std::cout << "De nette stapel: " << s[2] << std::endl;
-
-        std::cout << "De nette stapel met iterator: " << std::endl;
+        /*std::cout << "De nette stapel met iterator: " << std::endl;
         for (auto it = s[2].begin(); it != s[2].end(); ++it)
             std::cout << *it << "; ";
         std::cout << std::endl;
@@ -47,6 +53,7 @@ int main(int argc, char* argv[]) {
          }
         std::cout << std::endl;
         std::cout << "De nette stapel bevat nu nog: " << s[2] << std::endl;
+        */
 
 		return 0;
 	} catch(std::exception& e) {
