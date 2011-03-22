62ae6a42d530c2f4153b5eb74ba2d83d96017d6d
Nick Overdijk <nick@dotsimplicity.net>
2011-03-23 00:49:18 +0100

    nice

--- 06 Backtracking/main.cpp
+++ 06 Backtracking/main.cpp
@@ -1,3 +1,8 @@
+/*
+	Joshua Moerman - 3009408
+	Nick Overdijk  - 3029832
+*/
+
 #include <iostream>
 #include <vector>
 #include <algorithm>
@@ -10,16 +15,22 @@ std::vector<std::vector<T>>
 search(T goal, Iterator begin, Iterator const& end, std::vector<T> const& trace = std::vector<T>(), T const& previous_result = T(1)){
 	typedef std::vector<T> trace_t;
 	typedef std::vector<trace_t> traces_t;
+
+	//No solution found, return an empty list
 	if(begin == end) return traces_t();
+
+	//Create new potential solution
 	T const current_result = previous_result**begin;
-	trace_t new_trace[2] = {trace, trace};
-	new_trace[1].push_back(*begin);
+	trace_t new_trace = trace;
+	new_trace.push_back(*begin);
+
+	//Was it a solution?
 	if(current_result == goal){
-		return traces_t{new_trace[1]};
+		return traces_t{new_trace};
 	} else {
 		++begin;
-		traces_t x = search(goal, begin, end, new_trace[0], previous_result);
-		traces_t y = search(goal, begin, end, new_trace[1], current_result);
+		traces_t x = search(goal, begin, end, trace, previous_result);
+		traces_t y = search(goal, begin, end, new_trace, current_result);
 		std::copy(x.begin(), x.end(), std::back_inserter(y));
 		return y;
 	}
@@ -27,9 +38,12 @@ search(T goal, Iterator begin, Iterator const& end, std::vector<T> const& trace 
 
 template <typename T>
 void AWESOME(){
+	//If you guess the random number you get a cookie.
 	typename std::vector<T> list{-1, 1, 2, 3, 4, 5, 6, 7, 11, 13, std::rand()};
+	//Execellent use of the comma operator to create this AWESOME oneliner.
 	auto result = search((std::cout << "Geef een getal op: ", *std::istream_iterator<T>(std::cin)), list.begin(), list.end());
 	for(auto it = result.begin(); it != result.end(); ++it){
+		//THE 1 AT THE END IS SO THAT YOU GET NICE OUTPUT FOR FREE YES.
 		std::copy(it->begin(), it->end(), std::ostream_iterator<T>(std::cout, "*")); std::cout << "1\n";
 	}
 }
